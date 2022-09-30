9151654380e74b159c75d3859834aa093367ba36
CappuCitti <90761268+CappuCitti@users.noreply.github.com>
2022-09-30 10:20:27 +0200

    punto 5

--- 02.2/angular-for/src/app/app.module.ts
+++ 02.2/angular-for/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { MacchinaComponent } from './macchina/macchina.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MacchinaComponent } from './macchina/macchina.component';
     MacchinaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
