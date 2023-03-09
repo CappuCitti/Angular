174d8ae3a4fbb8bfdcc56a1d87a071f9cb373bdf
CappuCitti <90761268+CappuCitti@users.noreply.github.com>
2023-03-09 15:00:19 +0100

    Fix

--- 02.1/openfoodfacts/src/app/product/product.component.ts
+++ 02.1/openfoodfacts/src/app/product/product.component.ts
@@ -6,17 +6,19 @@ import { ApiService } from 'src/services/api.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
   product!: Product;
-  id: string | undefined = "";
+  id: string | undefined = '';
+  loading: boolean = true;
+  ingredients: Array<string> = [];
 
   constructor(
     private api: ApiService,
     private router: Router,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -24,8 +26,17 @@ export class ProductComponent {
 
       if (!this.id) this.router.navigate(['/']);
       else {
-        this.api.product(this.id).subscribe(data => this.product = data.product);
+        this.api.product(this.id).subscribe((data) => {
+          this.product = data.product;
+          this.loading = false;
+
+          this.ingredients = this.product.ingredients.map((i) => i.text);
+        });
       }
     });
+  }
+
+  clean(l: Array<string>): Array<string> {
+    return l.map(i => i.replace(/[A-z]*\:/, ''));
   }
 }
